e1d567e5af86a86cfe57d1731c28b60045d7187b
braden <imbradn@protonmail.com>
2023-05-25 10:36:27 -0700

    Added database connection to startup and moved into main() func

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
-import Koa from 'koa';
-import Router from '@koa/router';
-import { AppController } from './app/controllers/app';
-import bodyParser from 'koa-bodyparser';
+import "dotenv/config";
+import Koa from "koa";
+import Router from "@koa/router";
+import { AppController } from "./app/controllers/app";
+import bodyParser from "koa-bodyparser";
+import { databaseInit } from "./database/providers/login";
 
-const app = new Koa();
-app.use(bodyParser({ enableTypes: ["json"]}));
+const main = async () => {
+  const app = new Koa();
+  app.use(bodyParser({ enableTypes: ["json"] }));
 
-const router = new Router();
+  await databaseInit();
 
-AppController.loadRoutes(router);
+  const router = new Router();
+  AppController.loadRoutes(router);
+  app.use(router.routes());
 
-app.use(router.routes());
+  app.listen({ port: 8000 });
+};
 
-
-app.listen({ port: 8000 });
+main();
