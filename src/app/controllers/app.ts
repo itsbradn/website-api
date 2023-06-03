f5242954785fc736e52d9bfef9eac8271227be6d
braden <imbradn@protonmail.com>
2023-06-03 11:49:33 -0700

    Removed doc routes (Now static front end)

--- src/app/controllers/app.ts
+++ src/app/controllers/app.ts
@@ -1,7 +1,6 @@
 import { Controller } from "../interfaces/controller";
-import { DocumentationController } from "./docs/controller";
 import { IndexRoute } from "./index";
 import { SessionController } from "./session/controller";
 import { UserController } from "./user/controller";
 
-export const AppController = new Controller('/api/v1', [IndexRoute], [], null, [UserController, SessionController, DocumentationController]);
+export const AppController = new Controller('/api/v1', [IndexRoute], [], null, [UserController, SessionController]);
--- src/app/controllers/docs/controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller } from "../../interfaces/controller";
-import { getPageRoute } from "./getPage";
-
-export const DocumentationController = new Controller(
-  "/docs",
-  [getPageRoute],
-  []
-);
