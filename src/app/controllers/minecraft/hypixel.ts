dcbd438ace28a5e1c10400ebd95a4f80e152a826
braden <imbradn@protonmail.com>
2023-06-12 10:13:54 -0700

    Added hypixel request wrapper (temp)

---
+++ src/app/controllers/minecraft/hypixel.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { Route } from "../../interfaces/route";
+
+export const getPlayerRoute: Route = {
+  path: "/hypixel/:uuid",
+  method: "get",
+  actions: [
+    async (ctx) => {
+      const uuid = ctx.req.params.uuid;
+      if (!uuid) {
+        return {
+          status: 401,
+          body: {
+            error: "Invalid player",
+          },
+        };
+      }
+
+      const hypixelReq = await axios.get(
+        "https://api.hypixel.net/player?uuid=" + uuid,
+        {
+          headers: {
+            "API-Key": process.env.HYPIXEL_KEY!,
+          },
+        }
+      );
+
+      if (hypixelReq.data.success !== true) {
+        return {
+          status: 401,
+          body: {
+            error: hypixelReq.data.cause,
+          },
+        };
+      }
+
+      return {
+        status: 200,
+        body: hypixelReq.data,
+      };
+    },
+  ],
+};
