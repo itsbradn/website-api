2e2462ea4ab538aeab87d5fb52904ef40a250e84
braden <imbradn@protonmail.com>
2023-06-13 12:26:39 -0700

    Fixed null ratio

--- src/services/number.ts
+++ src/services/number.ts
@@ -1,11 +1,12 @@
 export const parseWhole = (num?: any) => {
   if (!num) return 0;
-  if (typeof num === 'number' || typeof num === 'bigint') return Math.round(Number(num));
-  if (typeof num === 'string') return Math.round(parseInt(num));
-  if (typeof num === 'boolean') return num ? 1 : 0;
+  if (typeof num === "number" || typeof num === "bigint")
+    return Math.round(Number(num));
+  if (typeof num === "string") return Math.round(parseInt(num));
+  if (typeof num === "boolean") return num ? 1 : 0;
   return 0;
-}
+};
 
 export const parseWinLoss = (num1?: any, num2?: any) => {
-  return (parseWhole(num1) / parseWhole(num2));
-}
+  return parseWhole(num1) / parseWhole(num2) ?? 0;
+};
