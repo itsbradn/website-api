988d7754ecb63e9bf00ed24509167d0f823ff9a5
braden <imbradn@protonmail.com>
2023-06-17 09:08:18 -0700

    Added level to bedwars res

--- src/services/hypixel/bedwars.ts
+++ src/services/hypixel/bedwars.ts
@@ -5,6 +5,7 @@ export interface BedwarsStats {
   dreamGamesPlayed: number;
   overallGamesPlayed: number;
   experience: number;
+  level: number
   experienceGameRatio: number;
   experienceWinRatio: number;
   lootChests: number;
@@ -78,6 +79,7 @@ export const formatBedwarsStats = (
         parseWhole(bedwars["games_played_bedwars"]),
       overallGamesPlayed: parseWhole(bedwars["games_played_bedwars_1"]),
       experience: parseWhole(bedwars["Experience"]),
+      level: getLevelForExp(parseWhole(bedwars['Experience'])),
       experienceGameRatio: parseWinLoss(
         bedwars["Experience"],
         bedwars["games_played_bedwars"]
@@ -443,3 +445,66 @@ export const convertModePrefixToTitle = (
   if (prefix === "four_three_") return "3v3v3v3";
   return "4v4";
 };
+
+const EASY_LEVELS = 4;
+const EASY_LEVELS_XP = 7000;
+const XP_PER_PRESTIGE = 96 * 5000 + EASY_LEVELS_XP;
+const LEVELS_PER_PRESTIGE = 100;
+const HIGHEST_PRESTIGE = 10;
+
+
+/**
+ * The following code is provided by KAD7 on the hypixel forums :)
+ */
+function getExpForLevel(level: number){
+    if(level == 0) return 0;
+
+    var respectedLevel = getLevelRespectingPrestige(level);
+    if(respectedLevel > EASY_LEVELS){
+        return 5000;
+    }
+
+    switch(respectedLevel){
+        case 1:
+            return 500;
+        case 2:
+            return 1000;
+        case 3:
+            return 2000;
+        case 4:
+            return 3500;
+    }
+    return 5000;
+}
+
+/**
+ * The following code is provided by KAD7 on the hypixel forums :)
+ */
+function getLevelRespectingPrestige(level: number){
+    if(level > HIGHEST_PRESTIGE * LEVELS_PER_PRESTIGE){
+        return level - HIGHEST_PRESTIGE * LEVELS_PER_PRESTIGE;
+    }
+    else {
+        return level % LEVELS_PER_PRESTIGE;
+    }
+}
+
+/**
+ * The following code is provided by KAD7 on the hypixel forums :)
+ */
+function getLevelForExp(exp: number){
+    var prestiges = Math.floor(exp / XP_PER_PRESTIGE);
+    var level = prestiges * LEVELS_PER_PRESTIGE;
+    var expWithoutPrestiges = exp - (prestiges * XP_PER_PRESTIGE);
+
+    for(let i = 1; i <= EASY_LEVELS; ++i){
+        var expForEasyLevel = getExpForLevel(i);
+        if(expWithoutPrestiges < expForEasyLevel){
+            break;
+        }
+        level++;
+        expWithoutPrestiges -= expForEasyLevel;
+    }
+    //returns players bedwars level, remove the Math.floor if you want the exact bedwars level returned
+    return level + expWithoutPrestiges / 5000; 
+}
